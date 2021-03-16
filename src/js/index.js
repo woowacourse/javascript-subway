f6fbeeb4eb07af364e78a3d2e6e90ffaa96a5843
SunnyK <sunnyk5780@gmail.com>
2021-03-16 17:55:37 +0900

    refactor: 웹 표준을 고려하여 HTML 및 CSS 수정

--- src/js/index.js
+++ src/js/index.js
@@ -2,25 +2,39 @@ const headerTemplate = `
 <a href="/" class="text-black">
   <h1 class="text-center font-bold">🚇 지하철 노선도</h1>
 </a>
-<nav class="d-flex justify-center flex-wrap">
-  <a href="/pages/stations.html" class="my-1">
-    <button class="btn bg-white shadow mx-1">🚉 역 관리</button>
-  </a>
-  <a href="/pages/lines.html" class="my-1">
-    <button class="btn bg-white shadow mx-1">🛤️ 노선 관리</button>
-  </a>
-  <a href="/pages/sections.html" class="my-1">
-    <button class="btn bg-white shadow mx-1">🔁 구간 관리</button>
-  </a>
-  <a href="/pages/map.html" class="my-1">
-    <button class="btn bg-white shadow mx-1">🗺️ 전체 보기</button>
-  </a>
-  <a href="/pages/search.html" class="my-1">
-    <button class="btn bg-white shadow mx-1">🔎 길 찾기</button>
-  </a>
-  <a href="/pages/login.html" class="my-1">
-    <button class="btn bg-white shadow mx-1">👤 로그인</button>
-  </a>
+<nav>
+  <ul class="d-flex justify-center flex-wrap">
+    <li>
+      <a href="/pages/stations.html" class="my-1 btn bg-white shadow d-flex items-center text-sm">
+        🚉 역 관리
+      </a>
+    </li>
+    <li>
+      <a href="/pages/lines.html" class="my-1 btn bg-white shadow d-flex items-center text-sm">
+        🛤️ 노선 관리
+      </a>
+    </li>
+    <li>
+      <a href="/pages/sections.html" class="my-1 btn bg-white shadow d-flex items-center text-sm">
+        🔁 구간 관리
+      </a>
+    </li>
+    <li>
+      <a href="/pages/map.html" class="my-1 btn bg-white shadow d-flex items-center text-sm">
+        🗺️ 전체 보기
+      </a>
+    </li>
+    <li>
+      <a href="/pages/search.html" class="my-1 btn bg-white shadow d-flex items-center text-sm">
+        🔎 길 찾기
+      </a>
+    </li>
+    <li>
+      <a href="/pages/login.html" class="my-1 btn bg-white shadow d-flex items-center text-sm">
+        👤 로그인
+      </a>
+    </li>
+  </ul>
 </nav>`;
 
-document.querySelector("header").innerHTML = headerTemplate;
+document.querySelector('header').innerHTML = headerTemplate;
