e1c84c19db7e7b58833fa8bfde7055dd3dbd939d
2sooy <1401660@naver.com>
2021-03-16 17:10:57 +0900

    chore : 개발 서버 구축
    - html-webpack-plugin : HTML 파일 생성을 단순화하는 웹 팩 플러그인
    - webpack-dev-server : 프론트엔드 개발 환경

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -19,5 +20,22 @@ module.exports = {
       },
     ],
   },
+
+  devServer: {
+    host: '127.0.0.1',
+    contentBase: path.join(__dirname, '/'),
+    compress: true,
+    hot: true,
+    inline: true,
+    port: 5500,
+    open: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+
   mode: 'none',
 };
