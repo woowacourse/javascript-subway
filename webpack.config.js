ebc4b92fc987abdbdd1bf42ae9736652f3e5ee1c
ChayanRin <k852012@naver.com>
2021-03-16 14:48:24 +0900

    chore: webpack.config.js 구현

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname + "/build"),
+  },
+  mode: "none",
+}
