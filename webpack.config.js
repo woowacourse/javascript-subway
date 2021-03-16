3ae9e90fefbac4677f6c7ca53561d6666adc42ca
jho2301 <jho2301@gmail.com>
2021-03-16 17:12:17 +0900

    chore: webpack 세팅
    - babel-loader
    - dev-server
    - css-loader

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: '[chunkhash].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    hot: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        include: [path.resolve(__dirname, 'src/js')],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        include: [path.resolve(__dirname, 'src/css')],
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
