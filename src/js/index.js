0fe9693ae9393855573bddb2173a7c1183b80927
ddongule <mnk918p@gmail.com>
2021-03-17 17:43:56 +0900

    refactor: HTML을 Template Literal로 변경

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,24 @@
 import '../css/index.css';
+
+import { $ } from './utils/DOM.js';
+import { stationTemplate } from './templates/lines.js';
 import { headerTemplate, getNavButtonTemplate } from './templates/header.js';
 import { NAVIGATION } from './constants/header.js';
 
 const renderHeader = () => {
-  document.querySelector('header').innerHTML = headerTemplate;
-  document.querySelector('nav').innerHTML = Object.values(NAVIGATION)
+  $('header').innerHTML = headerTemplate;
+  $('nav').innerHTML = Object.values(NAVIGATION)
     .map(getNavButtonTemplate)
     .join('');
 };
 
+const renderStation = () => {
+  $('main').innerHTML = stationTemplate();
+};
+
 renderHeader();
+renderStation();
+
+const templates = {
+  '/station': stationTemplate,
+};
