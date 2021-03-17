f62f0a05a8f0ff61c2dbc97d73bd094db912b3e1
HyuuunjuKim <gus7wn@gmail.com>
2021-03-17 17:09:52 +0900

    chore: webpack 배포용 초기 환경 설정

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+const childProcess = require('child_process');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/js/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve('./dist'),
+  },
+  devServer: {
+    open: true,
+    port: 5500,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date: ${new Date().toLocaleDateString()}
+        Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+        Contributors: [ella, tyche]
+      `,
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
