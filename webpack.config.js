bcf64047fc983072f7b6b55eed6ff7174611489c
Tanney <dhxodms89@gmail.com>
2021-03-17 11:26:23 +0900

    chore: webpack 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/js/index.js",
+    main: ["./src/js/index.js", "./src/css/index.css"],
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
@@ -19,5 +25,11 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve("./dist"),
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
 };
