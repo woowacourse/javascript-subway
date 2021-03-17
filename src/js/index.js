740d00d1590139c81001594d3f584594e9cf27ca
yujo <bedro27@gmail.com>
2021-03-17 11:34:43 +0900

    feat: History API에 url을 저장하는 기능

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,39 @@
+import '../css/index.css';
+
 const headerTemplate = `
 <a href="/" class="text-black">
   <h1 class="text-center font-bold">🚇 지하철 노선도</h1>
 </a>
-<nav class="d-flex justify-center flex-wrap">
+<nav class="js-navigation d-flex justify-center flex-wrap">
   <a href="/pages/stations.html" class="my-1">
-    <button class="btn bg-white shadow mx-1">🚉 역 관리</button>
+    <button class="js-navigation__stations btn bg-white shadow mx-1">🚉 역 관리</button>
   </a>
   <a href="/pages/lines.html" class="my-1">
-    <button class="btn bg-white shadow mx-1">🛤️ 노선 관리</button>
+    <button class="js-navigation__lines btn bg-white shadow mx-1">🛤️ 노선 관리</button>
   </a>
   <a href="/pages/sections.html" class="my-1">
-    <button class="btn bg-white shadow mx-1">🔁 구간 관리</button>
+    <button class="js-navigation__sections btn bg-white shadow mx-1">🔁 구간 관리</button>
   </a>
   <a href="/pages/map.html" class="my-1">
-    <button class="btn bg-white shadow mx-1">🗺️ 전체 보기</button>
+    <button class="js-navigation__map btn bg-white shadow mx-1">🗺️ 전체 보기</button>
   </a>
   <a href="/pages/search.html" class="my-1">
-    <button class="btn bg-white shadow mx-1">🔎 길 찾기</button>
+    <button class="js-navigation__search btn bg-white shadow mx-1">🔎 길 찾기</button>
   </a>
   <a href="/pages/login.html" class="my-1">
-    <button class="btn bg-white shadow mx-1">👤 로그인</button>
+    <button class="js-navigation__login btn bg-white shadow mx-1">👤 로그인</button>
   </a>
 </nav>`;
 
-document.querySelector("header").innerHTML = headerTemplate;
+const App = () => {
+  document.querySelector('header').innerHTML = headerTemplate;
+  document.querySelector('header').addEventListener('click', (e) => {
+    e.preventDefault();
+    const url = e.target.closest('a').getAttribute('href');
+    history.pushState({ url }, null, url);
+  });
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  App();
+});
