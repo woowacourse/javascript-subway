60d28931ccb9d37712c849479e51ab290935e242
jho2301 <jho2301@gmail.com>
2021-03-17 14:28:43 +0900

    test: [역 관리] 메뉴를 클릭하면, 역 관리 페이지가 렌더된다.

--- src/js/index.js
+++ src/js/index.js
@@ -5,22 +5,22 @@ const headerTemplate = `
   <h1 class="text-center font-bold">🚇 지하철 노선도</h1>
 </a>
 <nav class="d-flex justify-center flex-wrap">
-  <a href="/pages/stations.html" class="my-1">
+  <a href="/pages/stations.html" id="stations-nav-link" class="my-1">
     <button class="btn bg-white shadow mx-1">🚉 역 관리</button>
   </a>
-  <a href="/pages/lines.html" class="my-1">
+  <a href="/pages/lines.html" id="lines-nav-link" class="my-1">
     <button class="btn bg-white shadow mx-1">🛤️ 노선 관리</button>
   </a>
-  <a href="/pages/sections.html" class="my-1">
+  <a href="/pages/sections.html" id="sections-nav-link" class="my-1">
     <button class="btn bg-white shadow mx-1">🔁 구간 관리</button>
   </a>
-  <a href="/pages/map.html" class="my-1">
+  <a href="/pages/map.html" id="map-nav-link" class="my-1">
     <button class="btn bg-white shadow mx-1">🗺️ 전체 보기</button>
   </a>
-  <a href="/pages/search.html" class="my-1">
+  <a href="/pages/search.html" id="search-nav-link" class="my-1">
     <button class="btn bg-white shadow mx-1">🔎 길 찾기</button>
   </a>
-  <a href="/pages/login.html" class="my-1">
+  <a href="/pages/login.html" id="login-nav-link" class="my-1">
     <button class="btn bg-white shadow mx-1">👤 로그인</button>
   </a>
 </nav>`;
