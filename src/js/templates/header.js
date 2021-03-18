226a06c2616e32801f38052af77fc78baa6e3df8
bigsaigon333 <likepeppermint@gmail.com>
2021-03-18 17:07:03 +0900

    feat(라우팅): page별 template 작성

---
+++ src/js/templates/header.js
@@ -0,0 +1,22 @@
+import { ROUTES } from '../constants/index.js';
+
+const HEADER_TEMPLATE = `
+<a href="${ROUTES.HOME}" class="text-black">
+  <h1 class="text-center font-bold">🚇 지하철 노선도</h1>
+</a>
+<nav class="d-flex justify-center flex-wrap">
+  <a href="${ROUTES.STATIONS}" class="my-1">
+    <button class="btn bg-white shadow mx-1">🚉 역 관리</button>
+  </a>
+  <a href="${ROUTES.LINES}" class="my-1">
+    <button class="btn bg-white shadow mx-1">🛤️ 노선 관리</button>
+  </a>
+  <a href=${ROUTES.SECTIONS} class="my-1">
+    <button class="btn bg-white shadow mx-1">🔁 구간 관리</button>
+  </a>
+  <a href=${ROUTES.LOGIN} class="my-1">
+    <button class="btn bg-white shadow mx-1">👤 로그인</button>
+  </a>
+</nav>`;
+
+export default HEADER_TEMPLATE;
---
+++ src/js/templates/index.js
@@ -0,0 +1,8 @@
+import HEADER_TEMPLATE from './header.js';
+import STATIONS_TEMPLATE from './stations.js';
+import LINES_TEMPLATE from './lines.js';
+import SECTIONS_TEMPLATE from './sections.js';
+import LOGIN_TEMPLATE from './login.js';
+import SIGN_UP_TEMPLATE from './signUp.js';
+
+export { HEADER_TEMPLATE, STATIONS_TEMPLATE, LINES_TEMPLATE, SECTIONS_TEMPLATE, LOGIN_TEMPLATE, SIGN_UP_TEMPLATE };
