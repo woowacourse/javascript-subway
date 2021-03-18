83fb29a0578508ee8326e2f13758b8b3087debeb
sunhpark42 <sunhpark42@gmail.com>
2021-03-18 22:50:31 +0900

    feat: show warning message if login fails

--- src/js/components/LoginForm.js
+++ src/js/components/LoginForm.js
@@ -1,9 +1,16 @@
-import { $ } from '../utils/dom.js';
+import { $, show } from '../utils/dom.js';
 import { render } from '../../js/router.js';
-
+import { loginRequest } from '../request.js';
 export default class LoginForm {
   constructor() {
+    this.$inputForm = $('#login-form');
+    this.$loginErrorWarning = $('#login-error-warning');
     this.$signupLink = $('#signup');
+
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
 
   init() {
@@ -11,12 +18,43 @@ export default class LoginForm {
   }
 
   bindEvents() {
-    this.$signupLink.addEventListener('click', (e) => {
-      e.preventDefault();
-      const path = e.target.getAttribute('href');
+    this.$signupLink.addEventListener('click', this.goSignupForm.bind(this));
+    this.$inputForm.addEventListener('submit', this.handleSubmit.bind(this));
+  }
 
-      history.pushState({ path }, null, path);
-      render(path);
-    });
+  goSignupForm(event) {
+    event.preventDefault();
+    const path = event.target.getAttribute('href');
+
+    history.pushState({ path }, null, path);
+    render(path);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const {
+      email: { value: email },
+      password: { value: password },
+    } = event.target.elements;
+
+    this.state = { email, password };
+
+    this.submitForm();
+  }
+
+  async submitForm() {
+    await loginRequest(this.state)
+      .then(() => {
+        const path = '/stations';
+
+        history.pushState({ path }, null, path);
+        render(path);
+      })
+      .catch(() => this.warnLoginError());
+  }
+
+  warnLoginError() {
+    show(this.$loginErrorWarning);
   }
 }
--- src/js/request.js
+++ src/js/request.js
@@ -18,3 +18,22 @@ export const signupRequest = async (data) => {
 
   return response;
 };
+
+export const loginRequest = async (data) => {
+  const url = `${HOST}/login/token`;
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }).then((res) => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res;
+  });
+
+  return response;
+};
