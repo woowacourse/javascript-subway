c8efebb19c536795160a930280da0f3e579ee336
zereight <fullstackmachinelearning@gmail.com>
2021-03-18 17:30:38 +0900

    chore: add plugin
    Co-authored-by: Kyle <mkitigy@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties'],
           },
         },
       },
