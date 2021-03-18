e83872540f610c2974fbc9fc3d7ecbf0fd6f3a68
sunhpark42 <sunhpark42@gmail.com>
2021-03-18 17:13:33 +0900

    test: edit password confirm test code

--- cypress/integration/subwayAuth.spec.js
+++ cypress/integration/subwayAuth.spec.js
@@ -18,10 +18,9 @@ describe('지하철 노선도 로그인 및 회원가입 테스트', () => {
     cy.get('#signup-submit-button').click();
 
     cy.wait('@signup').its('response.statusCode').should('eq', 500);
-    cy.get('#email-input-warning').should(
-      'have.text',
-      '중복된 이메일이 존재합니다.'
-    );
+    cy.get('#email-input-warning')
+      .should('have.text', '중복된 이메일이 존재합니다.')
+      .and('be.visible');
   });
 
   it('회원 가입 시 입력한 비밀번호와 재입력한 비밀번호가 다르면, 경고 문구를 표시한다.', () => {
@@ -31,12 +30,15 @@ describe('지하철 노선도 로그인 및 회원가입 테스트', () => {
     cy.get('#email').type('sunny_login@email.com');
     cy.get('#name').type('sunny');
     cy.get('#password').type('sunny_password');
-    cy.get('#password-confirm').type('sunny_password_diff');
-    cy.get('#signup-submit-button').click();
+    cy.get('#password-confirm').type('sunny_pass');
 
-    cy.get('#password-confirm-warning').should(
-      'have.text',
-      '비밀번호가 일치하지 않습니다.'
-    );
+    cy.get('#password-confirm-error')
+      .should('be.visible')
+      .and('have.text', '비밀번호가 일치하지 않습니다.');
+
+    cy.get('#password-confirm').type('word');
+    cy.get('#password-confirm-correct')
+      .should('be.visible')
+      .and('have.text', '비밀번호가 일치합니다.');
   });
 });
