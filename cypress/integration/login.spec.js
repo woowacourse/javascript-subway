c24aa766d01a9bce394fdee4827a6411a353ef25
ddongule <mnk918p@gmail.com>
2021-03-18 16:25:02 +0900

    test: 로그인 테스트 기능 수정

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -3,24 +3,16 @@ describe('subway', () => {
     cy.visit('/');
   });
 
-  const interceptRequest = (url) => {
-    cy.intercept({
-      url,
-    }).as('request');
-  };
-
   it('이메일과 비밀번호를 입력하여 로그인한다.', () => {
+    cy.intercept({ url: 'http://15.164.230.130:8080/login' }).as('login');
     cy.get('.js-header__link[href="/login"]').click();
 
     cy.get('#email').type('imtjsqls@naver.com');
     cy.get('#password').type('qwer1234');
 
     cy.get('button[name="submit"]').click();
+    cy.wait('@login');
 
-    interceptRequest();
-
-    cy.wait('@request');
-
-    // TODO: 로그인 후 행동 추가
+    // TODO: Response Token이 들어오는지, Logout 버튼이 생기는지 확인
   });
 });
