361eb5c5152a5f36af89dbff67e1e063db012a4b
sunhpark42 <sunhpark42@gmail.com>
2021-03-18 22:48:54 +0900

    test: show warning message if login fails

--- cypress/integration/subwayAuth.spec.js
+++ cypress/integration/subwayAuth.spec.js
@@ -41,4 +41,15 @@ describe('지하철 노선도 로그인 및 회원가입 테스트', () => {
       .should('be.visible')
       .and('have.text', '비밀번호가 일치합니다.');
   });
+
+  it('로그인에 실패했을 시 안내 문구를 표시한다.', () => {
+    cy.get('#login-nav-button').click();
+
+    cy.get('#email').type('zig10@email.com');
+    cy.get('#password').type('123merong');
+
+    cy.get('#login-error-warning')
+      .should('be.visible')
+      .and('have.text', '아이디, 패스워드를 확인하세요.');
+  });
 });
--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -4,3 +4,8 @@ export const SELECTOR = {
   NAVIGATION: '#navigation',
   CONTENT: '#content',
 };
+
+export const MESSAGES = {
+  SIGNUP_SUCCESS: '회원가입에 성공했습니다.',
+  LOGIN_FAIL: '아이디, 패스워드를 확인하세요.',
+};
