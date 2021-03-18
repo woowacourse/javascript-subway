8065ea1d073774e06de48be80f9a04bc800ae22c
365kim <365listener@gmail.com>
2021-03-18 11:00:41 +0900

    test: 뒤로가기 및 앞으로가기 테스트

--- cypress/integration/routing.spec.js
+++ cypress/integration/routing.spec.js
@@ -1,3 +1,5 @@
+import { ROUTES } from '../../src/js/constants/index.js';
+
 describe('라우팅 테스트', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -6,5 +8,18 @@ describe('라우팅 테스트', () => {
   it('역 관리 메뉴를 클릭하면 선택한 메뉴가 표시된다.', () => {
     cy.get('header nav a').contains('역 관리').click();
     cy.get('main').contains('역 관리').should('exist');
+    cy.location().should(({ pathname }) => {
+      expect(pathname).to.eq(ROUTES.STATIONS);
+    });
+
+    cy.go('back');
+    cy.location().should(({ pathname }) => {
+      expect(pathname).to.eq(ROUTES.HOME);
+    });
+
+    cy.go('forward');
+    cy.location().should(({ pathname }) => {
+      expect(pathname).to.eq(ROUTES.STATIONS);
+    });
   });
 });
