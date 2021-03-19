bea5660c61dcaba3ec13358e6d0fc5739dd2287a
Sunny.K <sunnyk5780@gmail.com>
2021-03-19 16:09:57 +0900

    test: 로그인테스트용 계정 변경 및 오타 수정

--- cypress/integration/memberManagement.spec.js
+++ cypress/integration/memberManagement.spec.js
@@ -4,30 +4,30 @@ describe("사용자 로그인 및 회원가입", () => {
   });
 
   const FAIL_TOKEN = "s";
-  const TEST_EMAIL = "test@test.com";
-  const TEST_PW = "q1w2e3r4";
+  const TEST_EMAIL = "sunccol@test.com";
+  const TEST_PW = "1q2w3e4r";
 
   it("로그인에 실패하면 입력창을 초기화하고 하단에 에러 메시지를 보여준다.", () => {
     cy.get("#email").type(TEST_EMAIL);
     cy.get("#password").type(TEST_PW + FAIL_TOKEN);
-    cy.get("#password").type({ enter });
+    cy.get("#password").type("{enter}");
 
     cy.get("#email").should("have.value", "");
     cy.get("#password").should("have.value", "");
     cy.get(".js-login-error").first().should("be.visible");
   });
 
-  it("로그인에 성공하면 네비게이션이 활성화 되고 전체보기 화면이 나타난다.", () => {
+  it("로그인에 성공하면 네비게이션이 활성화 되고 역관리 화면이 나타난다.", () => {
     cy.get("#email").type(TEST_EMAIL);
     cy.get("#password").type(TEST_PW);
-    cy.get("#password").type({ enter });
+    cy.get("#password").type("{enter}");
 
     cy.get("header > nav").should("be.visible");
-    cy.url().should("include", "/map.html");
+    cy.url().should("include", "/stations");
   });
 
   it("로그아웃 버튼을 누르면 로그인 화면으로 돌아간다.", () => {
     cy.get("#logout-button").click();
-    cy.url().should("include", "/login.html");
+    cy.url().should("include", "/login");
   });
 });
