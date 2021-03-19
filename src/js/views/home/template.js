45969934b0464a5156a7b8b8edda4aa3dbc6ab4c
365kim <365listener@gmail.com>
2021-03-19 18:39:46 +0900

    refactor: HTML 파일 수정, 이미지 경로 변경

--- src/js/views/home/template.js
+++ src/js/views/home/template.js
@@ -1,6 +1,8 @@
+import subwayEmoji from '../../../images/subway_emoji.png';
+
 const HOME_TEMPLATE = `<div class="d-flex flex-col">
 <div class="d-flex justify-center">
-  <img src="src/images/subway_emoji.png" width="200" />
+  <img src="${subwayEmoji}" width="200" />
 </div>
 <p class="mt-0 text-center">지하철 노선도 앱을 사용하기 위해서는 로그인이 필요합니다.</p>
 </div>`;
