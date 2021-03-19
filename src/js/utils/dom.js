420fe011d0da2afb47375c14235a50c97545f788
2sooy <1401660@naver.com>
2021-03-19 13:53:34 +0900

    feat: form 태그의 input 태그 value 가져오는 유틸 함수 구현 및 적용
    Co-authored-by: Chayan <hchayan196@gmail.com>

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,2 +1,16 @@
 export const $ = selector => document.querySelector(selector);
 export const $$ = selector => document.querySelectorAll(selector);
+
+export const getFormData = formCollections => {
+  const formData = Array.from(formCollections)
+    .filter(({ name }) => name)
+    .reduce(
+      (result, { name, value }) => ({
+        ...result,
+        [name]: value,
+      }),
+      {},
+    );
+
+  return formData;
+};
