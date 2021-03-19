8a448fae82a59582e9c2f2eb261663685d2f55d1
seojihwan <seojihwan213@naver.com>
2021-03-19 19:18:12 +0900

    feat: private 라우터 적용

--- src/js/components/login/index.js
+++ src/js/components/login/index.js
@@ -4,7 +4,7 @@ import mainTemplate from './template/main.js';
 import { BASE_URL, PATH } from '../../constants/url.js';
 import request from '../../utils/fetch.js';
 import Station from '../station/index.js';
-import { ROUTE } from '../../constants/route.js';
+import { LINK } from '../../constants/header.js';
 
 class Login extends Component {
   constructor(parentNode) {
@@ -36,12 +36,14 @@ class Login extends Component {
         const { accessToken } = await response.json();
         localStorage.setItem('accessToken', accessToken);
 
-        history.pushState({ route: ROUTE.STATION }, null, ROUTE.STATION);
+        history.pushState(
+          { route: LINK.STATION.ROUTE },
+          null,
+          LINK.STATION.ROUTE
+        );
         new Station(this.parentNode);
 
         // 로그아웃 버튼 만들기
-
-        $('a[herf="/login"]');
 
         console.log(response.status);
       } catch (error) {
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 // import '../css/index.css';
 import { $ } from './utils/DOM.js';
-import { NAVIGATION } from './constants/header.js';
-import { ROUTE } from './constants/route.js';
+import { LINK } from './constants/header.js';
 import { headerTemplate } from './components/header.js';
 import Login from './components/login/index.js';
 import Signup from './components/signup/index.js';
@@ -9,25 +8,37 @@ import Section from './components/section/index.js';
 import Line from './components/line/index.js';
 import Station from './components/station/index.js';
 
+const PrivateRouter = (Station) => {
+  if (localStorage.getItem('accessToken')) {
+    return Station;
+  }
+
+  return Login;
+};
+
 class App {
   constructor() {
     this.components = {
       // [ROUTE.HOME]: loginRequiredTemplate,
-      [ROUTE.STATION]: Station,
-      [ROUTE.LINE]: Line,
-      [ROUTE.SECTION]: Section,
-      // [ROUTE.MAP]: loginRequiredTemplate,
-      // [ROUTE.SEARCH]: loginRequiredTemplate,
-      [ROUTE.LOGIN]: Login,
-      [ROUTE.SIGNUP]: Signup,
+      [LINK.STATION.ROUTE]: PrivateRouter(Station),
+      [LINK.LINE.ROUTE]: PrivateRouter(Line),
+      [LINK.SECTION.ROUTE]: PrivateRouter(Section),
+      // [NAVIGATION.ROUTE.MAP]: loginRequiredTemplate,
+      // [NAVIGATION.ROUTE.SEARCH]: loginRequiredTemplate,
+      [LINK.LOGIN.ROUTE]: Login,
+      [LINK.SIGNUP.ROUTE]: Signup,
     };
 
     this.renderHeader();
     this.addEventListeners();
   }
 
+  isLogin() {
+    return localStorage.getItem('accessToken');
+  }
+
   renderHeader() {
-    $('.js-header').innerHTML = headerTemplate(NAVIGATION);
+    $('.js-header').innerHTML = headerTemplate(LINK);
   }
 
   render(route) {
@@ -44,6 +55,10 @@ class App {
       if (!anchor) return;
 
       e.preventDefault();
+
+      if (anchor.href === LINK.LOGOUT.ROUTE) {
+        localStorage.clear();
+      }
 
       const route = anchor.getAttribute('href');
       history.pushState({ route }, null, route);
