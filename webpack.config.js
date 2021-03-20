7fb469daa22afc02ac61bea50184622d203b6e38
Kyle <mkitigy@gmail.com>
2021-03-20 14:14:44 +0900

    chore: install plugin - @babel/plugin-transform-runtime

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,11 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties'],
+            plugins: [
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-transform-runtime',
+            ],
           },
         },
       },
