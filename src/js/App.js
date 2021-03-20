da0144557e1b7a30b3ed98f2e752fc5412f6471e
ChayanRin <k852012@naver.com>
2021-03-20 15:10:18 +0900

    refactor: App.js 리팩토링

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,7 @@ import Router from './router.js';
 import Home from './components/home/Home.js';
 import Login from './components/login/Login.js';
 import SignUp from './components/signup/Signup.js';
-import Header from './layouts/Header.js';
+import Header from './components/header/Header.js';
 
 import { render } from './renderer.js';
 import { getLocalStorageItem } from './utils/storage.js';
@@ -15,21 +15,18 @@ class App {
     this.isLoggedIn = false;
   }
 
-  async init() {
-    this.isLoggedIn = await this.isValidUserAccessToken();
+  init() {
     this.initRouter();
-    this.initDOM();
     this.mountComponent();
     this.switchURL('/');
   }
 
   async isValidUserAccessToken() {
     const userAccessToken = getLocalStorageItem('userAccessToken');
-
     if (!userAccessToken) return false;
 
     try {
-      const user = await request(BASE_URL + ACTIONS.USER, {
+      await request(BASE_URL + ACTIONS.USER, {
         headers: {
           Authorization: `Bearer ${userAccessToken}`,
           Accept: 'application/json',
@@ -46,16 +43,17 @@ class App {
   initRouter() {
     this.router = new Router();
     this.router.init();
-  }
-
-  initDOM() {
-    this.header = new Header({
-      switchURL: this.switchURL.bind(this),
-    });
-    // TODO : isLoggedIN init으로 옮기기
+    this.route = {
+      '/': () => this.home.init(this.isLoggedIn),
+      '/login': () => this.login.init(),
+      '/signup': () => this.signup.init(),
+    };
   }
 
   mountComponent() {
+    this.header = new Header({
+      switchURL: this.switchURL.bind(this),
+    });
     this.home = new Home();
     this.login = new Login({ switchURL: this.switchURL.bind(this) });
     this.signup = new SignUp({ switchURL: this.switchURL.bind(this) });
@@ -63,16 +61,9 @@ class App {
 
   async switchURL(href) {
     this.router.renderPage(href);
-
-    if (href === '/') {
-      this.isLoggedIn = await this.isValidUserAccessToken();
-      this.header.init(this.isLoggedIn);
-      this.home.init(this.isLoggedIn);
-    } else if (href === '/login') {
-      this.login.init();
-    } else if (href === '/signup') {
-      this.signup.init();
-    }
+    this.isLoggedIn = await this.isValidUserAccessToken();
+    this.header.init(this.isLoggedIn);
+    this.route[href]();
   }
 }
 
