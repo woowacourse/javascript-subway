1a6404797f12b449783b91d01a476774ba83a809
shinsehantan <shinsehantan@users.noreply.github.com>
2021-03-20 22:51:11 +0900

    refactor: 상수 처리 및 if 문을 오브젝트 리터럴로 변경
    Co-authored-by: dudtjr913 <dudtjr913@users.noreply.github.com>

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -4,6 +4,7 @@ import Sections from './Sections.js';
 import SignIn from './SignIn.js';
 import SignUp from './SignUp.js';
 import Router from '../router/Router.js';
+import { ELEMENT, MENU_TITLE, PATH, SESSION_KEY_TOKEN } from '../utils/constants.js';
 
 class App {
   constructor() {
@@ -21,10 +22,10 @@ class App {
   }
 
   selectDom() {
-    this.$app = document.querySelector('#app');
-    this.$mainScreen = document.querySelector('.main-screen');
-    this.$signInButton = document.querySelector('.nav-bar__signin-button');
-    this.$$mainMenuButtons = document.querySelectorAll('.nav-bar .main__menu-router');
+    this.$app = document.querySelector(`.${ELEMENT.APP}`);
+    this.$mainScreen = document.querySelector(`.${ELEMENT.MAIN_SCREEN}`);
+    this.$signInButton = document.querySelector(`.${ELEMENT.NAV_BAR_SIGN_IN_BUTTON}`);
+    this.$$mainMenuButtons = document.querySelectorAll(`.${ELEMENT.NAV_BAR} .${ELEMENT.MAIN_MENU_ROUTER}`);
   }
 
   bindEvent() {
@@ -41,38 +42,44 @@ class App {
   }
 
   isRouterButton(target) {
-    return target.matches('.main__menu-router');
+    return target.matches(`.${ELEMENT.MAIN_MENU_ROUTER}`);
   }
 
   async handleSelectMenu(e) {
     const path = e.target.closest('a').getAttribute('href');
     await this.router.route(path);
 
-    if (path === '/signin') {
-      this.signIn.init();
-    }
+    this.runPathMatchedAction(path);
+  }
 
-    if (path === '/signup') {
-      this.signUp.init();
-    }
+  runPathMatchedAction(path) {
+    const pathActions = {
+      [PATH.SIGNIN]: () => {
+        this.signIn.init();
+      },
+      [PATH.SIGNUP]: () => {
+        this.signUp.init();
+      },
+      [PATH.SIGNOUT]: () => {
+        this.changeSignOutToSignInStatus();
+        this.router.route(PATH.MAIN);
+      },
+    };
 
-    if (path === '/signout') {
-      this.changeSignOutToSignInStatus();
-      this.router.route('/');
-    }
+    pathActions[path]?.();
   }
 
   changeSignInToSignOutStatus(accessToken) {
-    sessionStorage.setItem('token', accessToken);
-    this.$signInButton.innerText = '🙅🏻 로그아웃';
-    this.$signInButton.closest('a').href = '/signout';
+    sessionStorage.setItem(SESSION_KEY_TOKEN, accessToken);
+    this.$signInButton.innerText = MENU_TITLE.SIGN_OUT;
+    this.$signInButton.closest('a').href = PATH.SIGNOUT;
     this.showMenuButton();
   }
 
   changeSignOutToSignInStatus() {
-    sessionStorage.removeItem('token');
-    this.$signInButton.innerText = '🙆🏻 로그인';
-    this.$signInButton.closest('a').href = '/signin';
+    sessionStorage.removeItem(SESSION_KEY_TOKEN);
+    this.$signInButton.innerText = MENU_TITLE.SIGN_IN;
+    this.$signInButton.closest('a').href = PATH.SIGNIN;
     this.hideMenuButton();
   }
 
@@ -84,7 +91,7 @@ class App {
 
   hideMenuButton() {
     this.$$mainMenuButtons.forEach((button) => {
-      !button.classList.contains('nav-bar__signin-button') && button.classList.add('d-none');
+      !button.classList.contains(`${ELEMENT.NAV_BAR_SIGN_IN_BUTTON}`) && button.classList.add('d-none');
     });
   }
 }
