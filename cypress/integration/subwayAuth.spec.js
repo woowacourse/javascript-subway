3c1337e5993722e6b4bc2e3ba38d9c74dcfd0135
zigsong <wldms5764@gmail.com>
2021-03-20 22:31:50 +0900

    test(fix): check email duplicated & login failed

--- cypress/integration/subwayAuth.spec.js
+++ cypress/integration/subwayAuth.spec.js
@@ -6,21 +6,21 @@ describe('지하철 노선도 로그인 및 회원가입 테스트', () => {
   });
 
   it('회원 가입 시 중복된 이메일이 존재하면, 경고 문구를 표시한다.', () => {
-    cy.intercept('POST', `${requestURL}/members`).as('signup');
+    cy.intercept(
+      'GET',
+      `${requestURL}/members/check-validation?email=sunny_login%40email.com`
+    ).as('checkDuplicate');
 
     cy.get('#login-nav-button').click();
     cy.get('#signup').click();
 
     cy.get('#email').type('sunny_login@email.com');
-    cy.get('#name').type('sunny');
-    cy.get('#password').type('sunny_password');
-    cy.get('#password-confirm').type('sunny_password');
-    cy.get('#signup-submit-button').click();
+    cy.get('#duplicate-check-btn').click();
+    cy.wait('@checkDuplicate');
 
-    cy.wait('@signup').its('response.statusCode').should('eq', 500);
     cy.get('#email-input-error')
-      .should('have.text', '중복된 이메일이 존재합니다.')
-      .and('be.visible');
+      .should('be.visible')
+      .and('have.text', '중복된 이메일이 존재합니다.');
   });
 
   it('회원 가입 시 입력한 비밀번호와 재입력한 비밀번호가 다르면, 경고 문구를 표시한다.', () => {
@@ -42,11 +42,16 @@ describe('지하철 노선도 로그인 및 회원가입 테스트', () => {
       .and('have.text', '비밀번호가 일치합니다.');
   });
 
-  it('로그인에 실패했을 시 안내 문구를 표시한다.', () => {
+  it.only('로그인에 실패했을 시 안내 문구를 표시한다.', () => {
+    cy.intercept('POST', `${requestURL}/login/token`).as('login');
+
     cy.get('#login-nav-button').click();
 
     cy.get('#email').type('zig10@email.com');
     cy.get('#password').type('123merong');
+
+    cy.get('#login-submit').click();
+    cy.wait('@login');
 
     cy.get('#login-error-warning')
       .should('be.visible')
