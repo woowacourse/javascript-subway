bc427df009695a876256108ddc370e2b60867362
365kim <365listener@gmail.com>
2021-03-21 19:58:30 +0900

    test(라우팅): 변경된 구조 반영

--- cypress/integration/routing.spec.js
+++ cypress/integration/routing.spec.js
@@ -1,12 +1,22 @@
 import { ROUTES } from '../../src/js/constants/index.js';
 
 describe('라우팅 테스트', () => {
+  const oldUser = {
+    name: '하루',
+    email: '365kim@gmail.com',
+    password: '365',
+  };
+
   beforeEach(() => {
     cy.visit('/');
+    cy.get(`a[href="${ROUTES.LOGIN}"]`).click();
+    cy.get('#email').type(oldUser.email);
+    cy.get('#password').type(oldUser.password);
+    cy.get('button[type="submit"]').click();
   });
 
   it('역 관리 메뉴를 클릭하면 선택한 메뉴가 표시된다.', () => {
-    cy.get('header nav a').contains('역 관리').click();
+    cy.contains('역 관리').click();
     cy.get('main').contains('역 관리').should('exist');
     cy.location().should(({ pathname }) => {
       expect(pathname).to.eq(ROUTES.STATIONS);
@@ -14,7 +24,7 @@ describe('라우팅 테스트', () => {
 
     cy.go('back');
     cy.location().should(({ pathname }) => {
-      expect(pathname).to.eq(ROUTES.HOME);
+      expect(pathname).to.eq(ROUTES.LOGIN);
     });
 
     cy.go('forward');
