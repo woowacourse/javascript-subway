f9a95453e3ba02956ee3a3b4f40eeabbce09f535
Kyle <mkitigy@gmail.com>
2021-03-22 17:23:01 +0900

    refactor: refactoring - constants - template

--- src/js/subway/views/templates/stationManage.js
+++ src/js/subway/views/templates/stationManage.js
@@ -1,11 +1,32 @@
 import { MENU } from '../../constants/constants';
 
+const stationInfo = ({ name }) => `
+  <li class="station-list-item d-flex items-center py-2">
+    <span class="w-100 pl-2">${name}</span>
+    <button
+      type="button"
+      class="jse-update-button bg-gray-50 text-gray-500 text-sm mr-1"
+      >
+      수정
+    </button>
+    <button
+      type="button"
+      class="js-remove-button bg-gray-50 text-gray-500 text-sm"
+    >
+      삭제
+    </button>
+  </li>
+  <hr class="my-0" />
+  `;
+
+export const stationList = stations => stations.map(station => stationInfo(station)).join('');
+
 export const stationManage = `
   <div id="content" class="wrapper bg-white p-10">
     <div class="heading">
       <h2 class="mt-1">${MENU.STATIONS}</h2>
     </div>
-    <form>
+    <form id="station-add-form">
       <div class="d-flex w-100">
         <label for="station-name" class="input-label" hidden>
           역 이름
@@ -19,44 +40,23 @@ export const stationManage = `
           required
         />
         <button
-          type="button"
+          type="submit"
+          id="station-add-button"
           name="submit"
           class="input-submit bg-cyan-300 ml-2"
+          disabled
         >
           확인
         </button>
       </div>
+      <div
+        id="fail-message-box"
+        class="js-message-box message-box hidden mt-1 text-red mb-1 text-center"
+        >
+        ${MESSAGE.SIGNIN.FAIL}
+      </div>
     </form>
-    <ul class="mt-3 pl-0">
-      <li class="station-list-item d-flex items-center py-2">
-        <span class="w-100 pl-2">사당</span>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm mr-1"
-        >
-          수정
-        </button>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm"
-        >
-          삭제
-        </button>
-      </li>
-      <hr class="my-0" />
-      <li class="station-list-item d-flex items-center py-2">
-        <span class="w-100 pl-2">방배</span>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm mr-1"
-        >
-          수정
-        </button>
-        <button type="button" class="bg-gray-50 text-gray-500">
-          삭제
-        </button>
-      </li>
-      <hr class="my-0" />
+    <ul id="station-list" class="mt-3 pl-0">
     </ul>
   </div>
 `;
