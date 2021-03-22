cdf8e35032dbc25cafa19320f787bb6cfcf9c349
jho2301 <jho2301@gmail.com>
2021-03-22 20:38:57 +0900

    feat: api에러 시 error 페이지 구현
    Co-authored-by: Shim MunSeong <puterism.k@gmail.com>

---
+++ src/js/templates/error.js
@@ -0,0 +1,9 @@
+const errorPageTemplate = `
+  <div class="error-container d-flex items-center flex-col">
+    <h2 class="text-8xl">🤦🏻</h2>
+    <span>데이터를 불러오지 못했습니다</span>
+    <span>다시 시도하시려면 새로고침 버튼을 눌러주세요</span>
+  <div>
+`;
+
+export default errorPageTemplate;
