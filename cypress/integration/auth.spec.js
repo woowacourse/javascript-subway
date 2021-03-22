3fe48f11251fc8c435668c0b330b21f9dfe94715
seojihwan <seojihwan213@naver.com>
2021-03-23 00:53:09 +0900

    test: accessToken assertion 문제해결
    - url 변경
    - 오타 수정

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -1,29 +1,30 @@
-describe('subway', () => {
+import { BASE_URL, PATH } from '../../src/js/constants/url';
+
+describe('지하철 노선도 로그인, 로그아웃 기능 테스트', () => {
   before(() => {
     cy.visit('/');
+    localStorage.clear();
   });
 
   it('이메일과 비밀번호를 입력하여 로그인한다.', () => {
-    cy.intercept({ url: 'http://15.164.230.130:8080/login' }).as('login');
+    cy.intercept({ url: BASE_URL + PATH.MEMBERS.LOGIN }).as('login');
     cy.get('.js-link[href="/login"]').click();
 
     cy.get('#email').type('grooming@woowahan.com');
     cy.get('#password').type('123123');
 
     cy.get('button[name="submit"]').click();
-    cy.wait('@login').should(() => {
-      expect(localStorage.getItem('accessToken')).to.exist;
-    });
+    cy.wait('@login').its('response.body.accessToken').should('exist');
 
     cy.url().should('eq', Cypress.config().baseUrl + '/station');
   });
 
   it('로그인을 하게되면, 로그인 버튼이 로그아웃으로 바뀐다.', () => {
-    cy.get('.js-link[href="/logout]').should('be.visible');
+    cy.get('.js-link[href="/logout"]').should('be.visible');
   });
 
   it('로그아웃 버튼을 클릭하면, 로그아웃 된다.', () => {
-    cy.get('.js-link[href="/logout]').click();
-    cy.get('.js-link[href="/login]').should('be.visible');
+    cy.get('.js-link[href="/logout"]').click();
+    cy.get('.js-link[href="/login"]').should('be.visible');
   });
 });
--- src/js/constants/url.js
+++ src/js/constants/url.js
@@ -1,4 +1,3 @@
-// const BASE_URL = 'http://15.164.230.130:8080';
 const BASE_URL = 'http://3.35.213.149';
 const PATH = {
   MEMBERS: {
