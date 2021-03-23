a5dc50701642a55cf514cbe75957f5bb3abbfc5c
Tanney <dhxodms89@gmail.com>
2021-03-23 14:16:01 +0900

    refactor: fetch api 추상화

--- src/js/APIs/subwayAPI.js
+++ src/js/APIs/subwayAPI.js
@@ -1,50 +1,56 @@
-const END_POINT = "https://www.boorownie.com";
-const HEADERS = {
-  "Content-Type": "application/json; charset=UTF-8",
-};
 const PATH = {
   MEMBERS: "/members",
   LOGIN: "/login/token",
   CHECK_DUPLICATED_EMAIL: "/members/check-validation",
 };
 
-const getURL = (path, params = {}) => {
-  const queryString = Object.keys(params)
-    .map((key) => `${key}=${params[key]}`)
-    .join("&");
+const request = {
+  endPoint: "https://www.boorownie.com",
+  headers: {
+    "Content-Type": "application/json; charset=UTF-8",
+  },
 
-  return `${END_POINT}${path}${queryString === "" ? "" : `?${queryString}`}`;
+  createURL(path, params = {}) {
+    const queryString = Object.keys(params)
+      .map((key) => `${key}=${params[key]}`)
+      .join("&");
+
+    return `${this.endPoint}${path}${
+      queryString === "" ? "" : `?${queryString}`
+    }`;
+  },
+
+  async get(path, params = {}) {
+    const response = await fetch(this.createURL(path, params));
+
+    return response;
+  },
+
+  async post(path, body) {
+    const response = await fetch(path, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify(body),
+    });
+
+    return response;
+  },
 };
 
 export const checkDuplicatedEmailAPI = async (email) => {
-  const response = await fetch(getURL(PATH.CHECK_DUPLICATED_EMAIL, { email }));
+  const response = await request.get(PATH.CHECK_DUPLICATED_EMAIL, { email });
 
   return response;
 };
 
-export const signupAPI = async ({ email, password, name }) => {
-  const response = await fetch(getURL(PATH.MEMBERS), {
-    method: "POST",
-    headers: HEADERS,
-    body: JSON.stringify({
-      email,
-      password,
-      name,
-    }),
-  });
+export const signupAPI = async (memberInfo) => {
+  const response = await request.post(PATH.MEMBERS, memberInfo);
 
   return response;
 };
 
-export const loginAPI = async ({ email, password }) => {
-  const response = await fetch(getURL(PATH.LOGIN), {
-    method: "POST",
-    headers: HEADERS,
-    body: JSON.stringify({
-      email,
-      password,
-    }),
-  });
+export const loginAPI = async (loginInfo) => {
+  const response = await request.post(PATH.LOGIN, loginInfo);
 
   return response;
 };
