e97cb0aae91b8c24c5b31498c841e07da207bda3
jho2301 <jho2301@gmail.com>
2021-03-23 15:47:47 +0900

    feat: 삭제 시 confirm을 이용하여 한 번 더 유저가 확인할 수 있어야 한다.

--- src/js/api/httpClient.js
+++ src/js/api/httpClient.js
@@ -13,6 +13,7 @@ const fetchOption = (method, body, accessToken = '') => ({
 const httpClient = {
   get: path => fetch(API.BASE_URL + path, fetchOption(API.HTTP_METHOD.GET, null, accessToken.get())),
   post: (path, body) => fetch(API.BASE_URL + path, fetchOption(API.HTTP_METHOD.POST, body, accessToken.get())),
+  put: (path, body) => fetch(API.BASE_URL + path, fetchOption(API.HTTP_METHOD.PUT, body, accessToken.get())),
   delete: path => fetch(API.BASE_URL + path, fetchOption(API.HTTP_METHOD.DELETE, null, accessToken.get())),
 };
 
--- src/js/eventHandlers/handleStationStatus.js
+++ src/js/eventHandlers/handleStationStatus.js
@@ -4,10 +4,12 @@ import station from '../store/station';
 
 const handleStationStatus = async event => {
   if (event.target.classList.contains('js-station-delete-button')) {
+    if (!window.confirm(ALERT_MESSAGE.DELETE_STATION_CONFIRM)) return;
+
     const $targetStation = event.target.closest('.station-list-item');
     const stationID = $targetStation.dataset.id;
-    const result = await requestDeleteStation(stationID);
 
+    const result = await requestDeleteStation(stationID);
     if (!result.success) {
       alert(result.message);
       return;
