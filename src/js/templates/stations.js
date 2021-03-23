dfe4ee57aac7eab412f6d2d47979cd3e52499be7
jho2301 <jho2301@gmail.com>
2021-03-23 15:01:01 +0900

    test: 지하철역을 삭제할 수 있다

--- src/js/templates/stations.js
+++ src/js/templates/stations.js
@@ -4,7 +4,7 @@ export const stationListItemTemplate = ({ id, name }) => `
   <li class="station-list-item d-flex items-center py-2" data-id="${id}" data-name="${name}">
     <span class="w-100 pl-2">${name}</span>
     <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1">수정</button>
-    <button type="button" class="bg-gray-50 text-gray-500 text-sm">삭제</button>
+    <button type="button" class="bg-gray-50 text-gray-500 text-sm js-station-delete-button">삭제</button>
   </li>
   <hr class="my-0" />
 `;
