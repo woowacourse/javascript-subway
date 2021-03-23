eaf523ab307938e8c569d9b4d7cee5d3f4a1880a
sunhpark42 <sunhpark42@gmail.com>
2021-03-23 18:55:33 +0900

    feat: validate station name

--- src/js/components/StationManager/index.js
+++ src/js/components/StationManager/index.js
@@ -18,7 +18,35 @@ export default class StationManager {
     this.$content.innerHTML = stationTemplate;
   }
 
-  selectDOM() {}
+  selectDOM() {
+    this.stationNameForm = $('#station-name-form');
+    this.stationNameInput = $('#station-name');
+  }
 
-  bindEvents() {}
+  bindEvents() {
+    this.stationNameInput.addEventListener('input', () => {
+      this.setInputValidity();
+    });
+
+    this.stationNameForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.stationNameInput.reportValidity();
+    });
+  }
+
+  setInputValidity() {
+    const validityState = this.stationNameInput.validity;
+
+    if (validityState.valueMissing) {
+      this.stationNameInput.setCustomValidity('역 이름을 입력해 주세요.🙀');
+    } else if (validityState.tooShort) {
+      this.stationNameInput.setCustomValidity('2글자 이상 입력해 주세요.👾');
+    } else if (validityState.patternMismatch) {
+      this.stationNameInput.setCustomValidity(
+        '공백, 특수문자를 제외한 한글을 입력해 주세요.🤓'
+      );
+    } else {
+      this.stationNameInput.setCustomValidity('');
+    }
+  }
 }
--- src/js/components/StationManager/template.js
+++ src/js/components/StationManager/template.js
@@ -3,7 +3,7 @@ export default `
     <div class="heading">
       <h2 class="mt-1">🚉 역 관리</h2>
     </div>
-    <form>
+    <form id="station-name-form">
       <div class="d-flex w-100">
         <label for="station-name" class="input-label" hidden>
           역 이름
@@ -14,43 +14,60 @@ export default `
           name="stationName"
           class="input-field"
           placeholder="역 이름"
+          minlength="2"
+          maxlength="20"
+          pattern="[가-힣]+||[^A-Za-z0-9 !@#$%^&*]"
+          autocomplete="off" 
           required
+          autofocus
         />
         <button
-          type="button"
+          id="station-name-submit"
+          type="submit"
           name="submit"
           class="input-submit bg-cyan-300 ml-2"
         >
           확인
         </button>
       </div>
+      <p 
+        id="station-duplicated-warning" 
+        class="text-xs text-red w-100 ml-5 my-1 d-none"
+      >지하철 역이 이미 존재합니다.</p>
     </form>
     <ul class="mt-3 pl-0">
       <li class="station-list-item d-flex items-center py-2">
-        <span class="w-100 pl-2">사당</span>
+        <span class="station-item-name w-100 pl-2">사당</span>
         <button
           type="button"
           class="bg-gray-50 text-gray-500 text-sm mr-1"
+          data-action="edit"
         >
           수정
         </button>
         <button
           type="button"
           class="bg-gray-50 text-gray-500 text-sm"
+          data-action="delete"
         >
           삭제
         </button>
       </li>
       <hr class="my-0" />
       <li class="station-list-item d-flex items-center py-2">
-        <span class="w-100 pl-2">방배</span>
+        <span class="station-item-name w-100 pl-2">방배</span>
         <button
           type="button"
           class="bg-gray-50 text-gray-500 text-sm mr-1"
+          data-action="edit"
         >
           수정
         </button>
-        <button type="button" class="bg-gray-50 text-gray-500">
+        <button 
+          type="button" 
+          class="bg-gray-50 text-gray-500"
+          data-action="delete"
+        >
           삭제
         </button>
       </li>
