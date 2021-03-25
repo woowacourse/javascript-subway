39eb6f101784306003c17dd6146516f6b6b29e91
Kyle <mkitigy@gmail.com>
2021-03-25 12:17:11 +0900

    chore: update webpack config - babel plugin optional chaining

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,7 @@ module.exports = {
               '@babel/plugin-proposal-object-rest-spread',
               '@babel/plugin-proposal-class-properties',
               '@babel/plugin-transform-runtime',
+              '@babel/plugin-proposal-optional-chaining',
             ],
           },
         },
