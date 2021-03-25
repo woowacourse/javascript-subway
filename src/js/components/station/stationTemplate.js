c848b87407521f9601cb961ada077837961deca7
Chayan <chayans999@gmail.com>
2021-03-25 17:07:04 +0900

    test: 역 관리 관련 테스트 코드 작성 및 테스트 코드 파일 분리

--- src/js/components/station/stationTemplate.js
+++ src/js/components/station/stationTemplate.js
@@ -6,19 +6,20 @@ export const stationTemplate = () => {
     </div>
     <form>
       <div class="d-flex w-100">
-        <label for="station-name" class="input-label" hidden>
+        <label for="station-add-input" class="input-label" hidden>
           역 이름
         </label>
         <input
           type="text"
-          id="station-name"
-          name="stationName"
+          id="station-add-input"
+          name="station-add-input"
           class="input-field"
           placeholder="역 이름"
           required
         />
         <button
           type="button"
+          id="station-add-button"
           name="submit"
           class="input-submit bg-cyan-300 ml-2"
         >
@@ -26,7 +27,7 @@ export const stationTemplate = () => {
         </button>
       </div>
     </form>
-    <ul class="mt-3 pl-0">
+    <ul id="station-list" class="mt-3 pl-0">
       <li class="station-list-item d-flex items-center py-2">
         <span class="w-100 pl-2">사당</span>
         <button
@@ -47,15 +48,52 @@ export const stationTemplate = () => {
         <span class="w-100 pl-2">방배</span>
         <button
           type="button"
-          class="bg-gray-50 text-gray-500 text-sm mr-1"
+          class="modify-button bg-gray-50 text-gray-500 text-sm mr-1"
         >
           수정
         </button>
-        <button type="button" class="bg-gray-50 text-gray-500">
+        <button type="button" class="delete-button bg-gray-50 text-gray-500">
           삭제
         </button>
       </li>
       <hr class="my-0" />
     </ul>
   </div>`;
+};
+
+export const modalTemplate = () => {
+  return `
+  <div class="modal-inner p-8">
+  <button class="modal-close">
+    <svg viewbox="0 0 40 40">
+      <path class="close-x" d="M 10,10 L 30,30 M 30,10 L 10,30" />
+    </svg>
+  </button>
+  <header>
+    <h2 class="text-center">🔁 역 수정</h2>
+  </header>
+  <form name="modify-station">
+    <div class="input-control">
+      <label for="station-modify-input" class="input-label" hidden>역 가</label>
+      <input
+        id="station-modify-input"
+        name="station-modify-input"
+        class="input-field"
+        placeholder="변경할 역명을 작섷해주세요."
+        required
+      />
+    </div>
+    <div class="d-flex justify-end mt-3">
+      <button
+        id="station-modify-button"
+        type="submit"
+        name="submit"
+        class="input-submit bg-cyan-300"
+      >
+        확인
+      </button>
+    </div>
+  </form>
+</div>
+  `;
 };
