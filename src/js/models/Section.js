1ea8338ffb14374083706b4b1c06ff6389454bd9
sunhpark42 <sunhpark42@gmail.com>
2021-03-26 21:48:36 +0900

    feat: create Section model and Line template

---
+++ src/js/models/Section.js
@@ -0,0 +1,8 @@
+export default class Section {
+  constructor(upStation, downStation, distance, duration) {
+    this._upStation = upStation; // Station
+    this._downStation = downStation; // Station
+    this._distance = distance;
+    this._duration = duration;
+  }
+}
