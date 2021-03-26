099fd9e35c7a4e2c0c625b8aad3ee11687492cda
2sooy <1401660@naver.com>
2021-03-26 15:30:37 +0900

    refactor: 로그인 폼 검증 로직 switch 분기에서 if 분기로 변경

--- src/js/components/login/loginValidator.js
+++ src/js/components/login/loginValidator.js
@@ -2,11 +2,11 @@ import { ERROR_MESSAGE } from '../../constants';
 import { isEmptyString } from '../../utils/validation';
 
 export const checkLoginValid = ({ email, password }) => {
-  switch (true) {
-    case isEmptyString(email):
-      return ERROR_MESSAGE.EMPTY_EMAIL;
+  if (isEmptyString(email)) {
+    return ERROR_MESSAGE.EMPTY_EMAIL;
+  }
 
-    case isEmptyString(password):
-      return ERROR_MESSAGE.EMPTY_PASSWORD;
+  if (isEmptyString(password)) {
+    return ERROR_MESSAGE.EMPTY_PASSWORD;
   }
 };
