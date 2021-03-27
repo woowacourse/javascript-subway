005eab643970dec1f20298fe61e277a0be38d194
zigsong <wldms5764@gmail.com>
2021-03-27 16:35:23 +0900

    feat: render stations in selected line

--- src/js/components/SectionManager/template.js
+++ src/js/components/SectionManager/template.js
@@ -12,29 +12,10 @@ export const contentTemplate = `
     <form class="d-flex items-center pl-1">
       <label for="subway-line" class="input-label" hidden>노선</label>
       <select id="subway-line" class="bg-blue-400">
-        <option>1호선</option>
-        <option>2호선</option>
-        <option>3호선</option>
-        <option>4호선</option>
+        <option value="" selected disabled hidden>구간 선택</option>
       </select>
     </form>
-    <ul class="mt-3 pl-0">
-      <li class="d-flex items-center py-2 relative">
-        <span class="w-100 pl-6">인천</span>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm mr-1"
-        >
-          수정
-        </button>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm"
-        >
-          삭제
-        </button>
-      </li>
-      <hr class="my-0" />
+    <ul id="section-station-list" class="mt-3 pl-0">
     </ul>
   </div>
 `;
--- src/js/models/Section.js
+++ src/js/models/Section.js
@@ -1,5 +1,5 @@
 export default class Section {
-  constructor(upStation, downStation, distance, duration) {
+  constructor({ upStation, downStation, distance, duration }) {
     this._upStation = upStation; // Station
     this._downStation = downStation; // Station
     this._distance = distance;
