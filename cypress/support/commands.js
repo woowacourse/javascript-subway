723f31db4d1de1769ca4a712f62c5eb47edf4a17
Tanney <dhxodms89@gmail.com>
2021-03-27 18:56:14 +0900

    test: 테스트 로그인, 노선 추가 커맨드 추가

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,16 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", () => {
+  sessionStorage.clear();
+
+  cy.get("#email").type("sunccol@test.com");
+  cy.get("#password").type("1q2w3e4r").type("{enter}");
+});
+
+Cypress.Commands.add("addLine", (line) => {
+  cy.get("[name='subway-line-name']").clear().type(line.name);
+  cy.get("#up-station").select(line.upStation);
+  cy.get("#down-station").select(line.downStation);
+  cy.get("#distance").clear().type(line.distance);
+  cy.get("#duration").clear().type(line.duration);
+  cy.get(`.${line.color}`).click();
+  cy.type("{enter}");
+});
