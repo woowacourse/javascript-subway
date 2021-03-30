18d4fe411a255db0216125df915a9ec1211b3b3d
ddongule <mnk918p@gmail.com>
2021-03-30 22:25:12 +0900

    feat: 구간 조회, 생성, 삭제기능 구현

--- src/js/components/section/index.js
+++ src/js/components/section/index.js
@@ -1,16 +1,89 @@
+import getSubwayState from '../../api/apis.js';
+import getFetchParams from '../../api/getFetchParams.js';
+import { CONFIRM_MESSAGE } from '../../constants/message.js';
+import { PATH } from '../../constants/url.js';
 import Component from '../../core/Component.js';
-import { mainTemplate } from './template/main.js';
+import { $ } from '../../utils/DOM.js';
+import request from '../../utils/request.js';
+import Modal from './modal.js';
+import { mainTemplate, sectionItem } from './template/main.js';
 
 class Section extends Component {
   constructor(parentNode, stateManagers) {
-    super(parentNode, stateManagers);
+    super(
+      parentNode,
+      stateManagers,
+      {
+        modal: new Modal($('.js-modal'), stateManagers),
+      },
+      {
+        stations: [],
+        lines: [],
+      }
+    );
+
+    this.setChildProps('modal', {
+      updateSubwayState: this.updateSubwayState.bind(this),
+    });
   }
 
   renderSelf() {
-    this.parentNode.innerHTML = mainTemplate();
+    this.parentNode.innerHTML = mainTemplate(this.state.lines);
   }
 
-  addEventListeners() {}
+  addEventListeners() {
+    $('.js-section-item__create').addEventListener('click', () => {
+      // create 로쥑
+      this.childComponents.modal.show();
+    });
+
+    $('.js-section-form__select').addEventListener('change', ({ target }) => {
+      const lineId = target.value;
+      const { color, stations } = this.state.lines.find(
+        (line) => line.id === Number(lineId)
+      );
+      console.log(stations);
+
+      $('.js-section-list').innerHTML = stations
+        .map((station) => sectionItem(station))
+        .join('');
+
+      $('.js-section-list').setAttribute('data-line-id', lineId);
+      $('.js-section-form__select').setAttribute('data-bg-color', color);
+    });
+
+    $('.js-section-list').addEventListener('click', async ({ target }) => {
+      console.log(target, '?????');
+      if (!target.classList.contains('js-section-item__delete')) return;
+      if (!confirm(CONFIRM_MESSAGE.DELETE)) return;
+
+      const lineId = target.closest('.js-section-list').dataset.lineId;
+      const stationId = target.closest('.js-section-item').dataset.stationId;
+      const accessToken = this.stateManagers.accessToken.getToken();
+
+      const params = getFetchParams({
+        path: `${PATH.LINES}/${lineId}/sections?stationId=${stationId}`,
+        accessToken,
+      });
+
+      try {
+        const response = await request.delete(params);
+
+        if (!response.ok) throw Error(await response.text());
+
+        await this.updateSubwayState();
+      } catch (error) {
+        console.error(error.message);
+      }
+    });
+  }
+
+  async updateSubwayState() {
+    const accessToken = this.stateManagers.accessToken.getToken();
+    const { stations, lines } = await getSubwayState(accessToken);
+
+    this.setState({ stations, lines });
+  }
 }
 
 export default Section;
