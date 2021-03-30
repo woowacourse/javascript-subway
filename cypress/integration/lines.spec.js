0eb79fa05bd013ddf9e90696d9e857c5eb861532
zigsong <wldms5764@gmail.com>
2021-03-30 18:59:58 +0900

    test: edit and delete subway line

--- cypress/integration/lines.spec.js
+++ cypress/integration/lines.spec.js
@@ -57,4 +57,51 @@ describe('지하철 노선 관리 테스트', () => {
     cy.get('#subway-line-name').type(newLineName).blur();
     cy.get('#line-duplicated-warning').should('be.visible').and('have.text', '노선이 이미 존재합니다.');
   });
+
+  it('지하철 노선 정보의 이름과 색상을 수정할 수 있다.', () => {
+    const newLineName = '우아한테크코스선55';
+    cy.intercept('PUT', `${requestURL}/lines`).as('editLine');
+
+    cy.get('.line-list-item')
+      .first()
+      .then((item) => {
+        cy.wrap(item)
+          .find('[data-action="edit"]')
+          .click()
+          .then(() => {
+            cy.get('.modal').should('be.visible');
+            cy.get('#subway-line-name').clear().type(newLineName);
+            cy.get('.subway-line-color-selector > .bg-indigo-500').click();
+            cy.get('#modal-create-line').click();
+            cy.wait('@editLine');
+            cy.get('.modal').should('not.be.visible');
+          });
+      });
+
+    cy.get('.line-list-item').first().find('.subway-line-list-item-name').should('have.text', newLineName);
+    cy.get('.line-list-item').first().find('.subway-line-color-dot').should('have.class', 'bg-indigo-500');
+  });
+
+  it('지하철 노선을 삭제할 수 있다.', () => {
+    const confirmStub = cy.stub();
+    const targetLineName = '우아한테크코스선55';
+
+    cy.intercept('DELETE', `${requestURL}/lines`).as('deleteLine');
+    cy.on('window:confirm', confirmStub);
+
+    cy.get('.line-list-item')
+      .first()
+      .then((item) => {
+        cy.wrap(item)
+          .find('[data-action="delete"]')
+          .click()
+          .then(() => {
+            expect(confirmStub.getCall(0)).to.be.calledWith(`${targetLineName}을 삭제하시겠습니까?`);
+          });
+      });
+
+    cy.wait('@deleteLine');
+    cy.get('.subway-line-list-item-name').contains(targetLineName).should('not.exist');
+    cy.get('.snackbar').should('have.text', `${targetLineName}이 삭제되었습니다.`);
+  });
 });
