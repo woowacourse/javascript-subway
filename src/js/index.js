fd35a911151f489c10267fdb3e7b6b0fe50543f9
sunhpark42 <sunhpark42@gmail.com>
2021-03-31 13:48:20 +0900

    feat: greet user when logged in

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import {
 import { getCookie } from './utils/cookie.js';
 import getAvailablePath from './utils/path.js';
 import popSnackbar from './utils/snackbar.js';
-import { MESSAGES } from './constants/constants.js';
+import { SELECTOR, MESSAGES } from './constants/constants.js';
 import {
   NavigationBar,
   EntryPage,
@@ -21,6 +21,7 @@ import {
 } from './components';
 import Station from './models/Station.js';
 import Line from './models/Line.js';
+import { $, show, hide } from './utils/dom.js';
 
 export default class App {
   constructor() {
@@ -68,7 +69,11 @@ export default class App {
   async update() {
     if (this.store.isLoggedIn) {
       await this.getPersonalSubwayData();
+      show($(SELECTOR.USER_GREET_MESSAGE));
+      $(SELECTOR.USER_GREET_NAME).textContent = this.store.userAuth.name;
+      return;
     }
+    hide($(SELECTOR.USER_GREET_MESSAGE));
   }
 
   async checkIsLoggedIn() {
