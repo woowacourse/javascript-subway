0cfe4b588ca0a7ba653055597dd00728cf5e32b0
bigsaigon333 <likepeppermint@gmail.com>
2021-03-31 16:07:14 +0900

    test(노선관리): 노선 관리 기능테스트 수정
    Co-authored-by: 365kim <365kim@users.noreply.github.com>

--- cypress/integration/lines.spec.js
+++ cypress/integration/lines.spec.js
@@ -2,44 +2,67 @@ import PATHNAMES from '../../src/js/constants/rawPathnames.js';
 import { API_ENDPOINT } from '../../src/js/constants/api.js';
 import { LINES_MESSAGES } from '../../src/js/constants/messages.js';
 
+const oldUser = {
+  name: '하루',
+  email: '365kim@gmail.com',
+  password: '365',
+};
+
+const testLineItem = {
+  name: '2호선',
+  upStation: '사당',
+  downStation: '잠실',
+  distance: 250,
+  duration: 70,
+};
+
+const addStation = (name) => {
+  cy.get('#add-station-name').type(name);
+  cy.get('.submit-button').click();
+  cy.wait('@createStation');
+  cy.wait('@readStation');
+};
+
+const addLineListItem = (item) => {
+  cy.get('#add-line-name').type(item.name);
+  cy.get('#add-up-station').select(item.upStation);
+  cy.get('#add-down-station').select(item.downStation);
+  cy.get('#add-distance').type(item.distance);
+  cy.get('#add-duration').type(item.duration);
+  cy.get('.submit-button').click();
+  cy.wait('@createLine');
+};
+
+const intercept = () => {
+  cy.intercept({
+    method: 'POST',
+    url: `${API_ENDPOINT.LOGIN}`,
+  }).as('login');
+
+  cy.intercept({
+    method: 'POST',
+    url: `${API_ENDPOINT.STATIONS}`,
+  }).as('createStation');
+
+  cy.intercept({
+    method: 'GET',
+    url: `${API_ENDPOINT.STATIONS}`,
+  }).as('readStation');
+
+  cy.intercept({
+    method: 'POST',
+    url: `${API_ENDPOINT.LINES}`,
+  }).as('createLine');
+
+  cy.intercept({
+    method: 'GET',
+    url: `${API_ENDPOINT.LINES}`,
+  }).as('readLine');
+};
+
 describe('노선 관리 기능 테스트', () => {
-  const oldUser = {
-    name: '하루',
-    email: '365kim@gmail.com',
-    password: '365',
-  };
-
-  const testLineItem = {
-    name: '2호선',
-    upStation: '사당',
-    downStation: '잠실',
-    distance: 250,
-    duration: 70,
-  };
-
-  const addStation = (name) => {
-    cy.get('#add-station-name').type(name);
-    cy.get('.submit-button').click();
-  };
-
-  const addLineListItem = (item) => {
-    cy.get('#add-line-name').type(item.name);
-    cy.get('#add-up-station').select(item.upStation);
-    cy.get('#add-down-station').select(item.downStation);
-    cy.get('#add-distance').type(item.distance);
-    cy.get('#add-duration').type(item.duration);
-    cy.get('.submit-button').click();
-  };
-
-  beforeEach(() => {
-    cy.intercept({
-      method: 'POST',
-      url: `${API_ENDPOINT.LOGIN}`,
-    }).as('login');
-
-    cy.window().then((window) => {
-      cy.stub(window, 'confirm').returns(true).as('window:confirm');
-    });
+  before(() => {
+    intercept();
 
     cy.visit('/');
     cy.get(`a[href*="${PATHNAMES.LOGIN}"]`).click();
@@ -54,14 +77,23 @@ describe('노선 관리 기능 테스트', () => {
     addLineListItem(testLineItem);
   });
 
-  it('노선 이름, 상행역, 하행역, 거리, 시간 입력하면 추가된다.', () => {
-    cy.get('.line-list-item').should('contain', testLineItem.name);
+  beforeEach(() => {
+    intercept();
   });
 
-  it('중복된 역 이름 추가 시도할 경우, 중복 알림 메세지가 스낵바로 표시된다.', () => {
+  it('노선 이름, 상행역, 하행역, 거리, 시간 입력하면 추가된다.', () => {
+    cy.get('.line-list-item input[type="text"]')
+      .first()
+      .then(($el) => {
+        const [$newLineName] = $el;
+
+        expect($newLineName.value).to.be.equal(testLineItem.name);
+      });
+  });
+
+  it('중복된 노선 이름 추가 시도할 경우, 중복 알림 메세지가 스낵바로 표시된다.', () => {
     addLineListItem(testLineItem);
 
-    cy.get('.line-list-item').first().should('contain', testLineItem.name);
     cy.get('#snackbar-container').should('contain', LINES_MESSAGES.LINE_NAME_ALREADY_EXISTS);
   });
 
@@ -74,24 +106,33 @@ describe('노선 관리 기능 테스트', () => {
       duration: 120,
     };
 
-    cy.get('.edit-button').click();
-    cy.get('.edit-line-name').type(editedItem.name);
-    cy.get('.edit-up-station').type(editedItem.upStation);
-    cy.get('.edit-down-station').type(editedItem.downStation);
-    cy.get('.edit-duration').type(editedItem.duration);
-    cy.get('.check-button').click();
+    cy.get('.edit-button').first().click();
+    cy.get('.edit-line-name').first().clear({ force: true }).type(editedItem.name);
+    cy.get('.check-button').first().click();
+    cy.wait('@readLine');
 
     cy.get('#snackbar-container').should('contain', LINES_MESSAGES.LINE_HAS_BEEN_UPDATED);
-    cy.get('.line-list-item').should('not.contain', testLineItem.name);
-    cy.get('.line-list-item').should('contain', editedItem.name);
+    cy.get('.line-list-item input[type="text"]')
+      .first()
+      .then(($el) => {
+        const [$newLineInput] = $el;
+
+        expect($newLineInput.value).to.be.equal(editedItem.name);
+      });
   });
 
   it('역의 편집버튼 클릭 후 삭제버튼을 클릭하면 confirm창이 뜨고, 확인을 누르면 노선이 삭제된다.', () => {
-    cy.get('.edit-button').click();
-    cy.get('.remove-button').click();
+    const stub = cy.stub();
+    cy.on('window:confirm', stub);
 
-    cy.get('@window:confirm').should('be.calledWith', LINES_MESSAGES.ARE_YOU_SURE_TO_REMOVE);
+    cy.get('.edit-button').first().click();
+    cy.get('.remove-button')
+      .first()
+      .click({ force: true })
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith(LINES_MESSAGES.ARE_YOU_SURE_TO_REMOVE);
+      });
+
     cy.get('#snackbar-container').should('contain', LINES_MESSAGES.LINE_HAS_BEEN_REMOVED);
-    cy.get('.line-list-item').should('not.contain', testLineItem.name);
   });
 });
