51c3dfcc3f6ba7448f8f22fcf35b2b13bf1c5543
sunhpark42 <sunhpark42@gmail.com>
2021-04-01 13:05:17 +0900

    feat: render all lines in map

--- src/js/components/MapPage/index.js
+++ src/js/components/MapPage/index.js
@@ -16,9 +16,58 @@ export default class MapPage {
 
   render() {
     this.$content.innerHTML = contentTemplate;
+    $('#line-select').append(...this.store.lines.map((line) => new Option(line.name, line.id)));
+
+    $('#subway-list').innerHTML = this.store.lines
+      .map(
+        (line) =>
+          `<li class="subway-line">
+            <h3 class="subway-line-name mb-0 ${line.color} text-base text-white font-medium">${line.name}</h3>
+            <ul class="station-list mt-0 px-4" data-line-id=${line.id} data-line-name="${line.name}" data-line-color="${line.color}">
+            </ul>
+          </li>`
+      )
+      .join('');
+
+    $('.station-list').forEach(($line) => {
+      $line.innerHTML = this.store.lines
+        .find((line) => line.id === Number($line.dataset.lineId))
+        .stations.map(
+          (station) => `
+            <li data-station-id="${station.id}" data-station-name="${station.name}" class="station-list-item d-flex items-center py-2">
+              <span class="vertical-line ${$line.dataset.lineColor}"></span>
+              <span class="subway-line-color-dot ${$line.dataset.lineColor}"></span>
+              <span class="station-item-name w-100 pl-6">${station.name}</span>
+            </li>
+            <hr class="my-0" />
+            `
+        )
+        .join('');
+    });
   }
 
-  selectDOM() {}
+  selectDOM() {
+    this.$lineSelect = $('#line-select');
+  }
 
-  bindEvents() {}
+  bindEvents() {
+    $('#line-select').addEventListener('change', this.handleLineSelect.bind(this));
+  }
+
+  handleLineSelect(event) {
+    this.lineID = event.target.value;
+
+    this.renderLineData();
+  }
+
+  renderLineData() {
+    const targetLine = this.store.lines.find((line) => line.id === Number(this.lineID));
+    const prevLineBorderColor = [...this.$lineSelect.classList].find((className) =>
+      className.startsWith('border-color-')
+    );
+
+    const extractedColor = targetLine ? targetLine.color.replace(/bg-/, '') : 'gray-100';
+    this.$lineSelect.classList.remove(prevLineBorderColor);
+    this.$lineSelect.classList.add(`border-color-${extractedColor}`);
+  }
 }
--- src/js/components/MapPage/template.js
+++ src/js/components/MapPage/template.js
@@ -3,6 +3,16 @@ export const contentTemplate = `
   <div class="heading">
     <h2 class="mt-1">전체 보기</h2>
   </div>
+
+  <form class="d-flex items-center pl-1">
+    <label for="subway-line" class="input-label" hidden>노선</label>
+    <select id="line-select" class="border-4 border-color-gray-100 bg-white">
+      <option selected>전체 보기</option>
+    </select>
+  </form>
+
+  <ul id="subway-list">
+  </ul>
 `;
 
 // TODO: 각 노선 별 역 전체 보기
