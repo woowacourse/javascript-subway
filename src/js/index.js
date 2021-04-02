ad1cbdad452c6dac4700278a7d8439767d656305
yungo1846 <yungo1468@naver.com>
2021-04-02 22:17:36 +0900

    docs: 3단계 기능 구현목록 추가

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,6 @@ import App from './components/App.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   new App();
-  console.log(window.location.pathname);
   const popStateEvent = new Event('popstate');
   window.dispatchEvent(popStateEvent);
 });
