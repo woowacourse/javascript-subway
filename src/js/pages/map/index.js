1cece6b051c1c4d9a2beb90b0f426dcee2117803
jho2301 <jho2301@gmail.com>
2021-04-02 21:51:24 +0900

    feat: 전체보기 기능

--- src/js/pages/map/index.js
+++ src/js/pages/map/index.js
@@ -1,8 +1,10 @@
+import store from '../../store';
 import { $ } from '../../utils/dom';
 import mapPageTemplate from './template';
 
 const mountMap = () => {
-  $('#route-container').innerHTML = mapPageTemplate;
+  $('#route-container').innerHTML = mapPageTemplate(store.line.get());
+  console.log(store.line.get());
 };
 
 export default mountMap;
--- src/js/pages/search/index.js
+++
@@ -1,8 +0,0 @@
-import searchPageTemplate from './template';
-import { $ } from '../../utils/dom';
-
-const mountSearch = () => {
-  $('#route-container').innerHTML = searchPageTemplate;
-};
-
-export default mountSearch;
