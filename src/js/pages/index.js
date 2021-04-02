08bc381ee9a29dbce9ab3b2aff0bfdd8a7adec84
jho2301 <jho2301@gmail.com>
2021-04-02 21:51:24 +0900

    feat: 전체보기 기능

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,5 +1,4 @@
 import mountLogin from './login';
-import mountSearch from './search';
 import mountSignup from './signup';
 import mountStations from './stations';
 import mountSections from './sections';
@@ -7,4 +6,4 @@ import mountMap from './map';
 import mountLines from './lines';
 import mountError from './error';
 
-export { mountLogin, mountError, mountSearch, mountSignup, mountStations, mountSections, mountLines, mountMap };
+export { mountLogin, mountError, mountSignup, mountStations, mountSections, mountLines, mountMap };
--- src/js/pages/search/template.js
+++
@@ -1,8 +0,0 @@
-const searchPageTemplate = `
-  <div class="d-flex justify-center mt-5 w-100">
-    <div class="w-100">
-    </div>
-  </div>
-`;
-
-export default searchPageTemplate;
