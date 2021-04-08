2bc7772867a08340e577eaf81576629ead4501b9
권세진 <0307kwon@gmail.com>
2021-04-08 18:33:42 +0900

    refactor: load 함수에서 api 요청과 render부분을 분리한다

--- src/js/components/FullMapComponent.js
+++ src/js/components/FullMapComponent.js
@@ -22,17 +22,20 @@ class FullMapComponent extends Component {
       const accessToken = localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN);
       const response = await fetchLineListRead(accessToken);
       const lines = await response.json();
-      const linesTemplate = lines
-        .map(line =>
-          FULL_MAP_TEMPLATE.makeLineTemplate(line.name, line.stations)
-        )
-        .join('');
 
-      $(`#${ID_SELECTOR.FULL_MAP_LINE_LIST}`).innerHTML = linesTemplate;
+      this.renderFullMap(lines);
     } catch (error) {
       this.props.treatFetchError(error);
     }
   };
+
+  renderFullMap(lines) {
+    const linesTemplate = lines
+      .map(line => FULL_MAP_TEMPLATE.makeLineTemplate(line.name, line.stations))
+      .join('');
+
+    $(`#${ID_SELECTOR.FULL_MAP_LINE_LIST}`).innerHTML = linesTemplate;
+  }
 }
 
 export default FullMapComponent;
