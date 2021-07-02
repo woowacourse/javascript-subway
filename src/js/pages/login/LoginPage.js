7db081574ae024b1679ae923be9989f95bed9eb5
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-07-03 04:25:35 +0900

    refactor: 로그인 페이지 리팩토링

--- src/js/pages/login/LoginPage.js
+++ src/js/pages/login/LoginPage.js
@@ -1,17 +1,18 @@
+import Cookies from 'js-cookie';
+
+import { fetchLogin } from '../../API/auth.js';
+
+import user from '../../models/user.js';
+import loginTemplate from './loginTemplate.js';
+
+import router from '../../router.js';
+
 import { $ } from '../../utils/DOM.js';
+import showSnackBar from '../../utils/snackbar.js';
+
 import { MESSAGE, SNACKBAR_MESSAGE } from '../../constants/messages.js';
 import { COOKIE_KEY } from '../../constants/constants.js';
 import { PATH } from '../../constants/path.js';
-import { fetchLogin } from '../../API/auth.js';
-import jwtToken from '../../jwtToken.js';
-import loginTemplate from './loginTemplate.js';
-import {
-  checkEmailInputHandler,
-  checkPasswordInputHandler,
-} from '../../authHandlers.js';
-import showSnackBar from '../../utils/snackbar.js';
-import user from '../../models/user.js';
-import router from '../../router.js';
 
 class LoginPage {
   init() {
@@ -25,25 +26,13 @@ class LoginPage {
     $('#main').innerHTML = loginTemplate;
   }
 
-  async requestLogin(request) {
-    try {
-      const response = await fetchLogin(request);
+  bindEvents() {
+    $('#login-form').addEventListener('submit', this.loginHandler.bind(this));
+    $('#signup').addEventListener('click', e => {
+      e.preventDefault();
 
-      if (!response.ok) {
-        throw MESSAGE.ERROR.CHECK_EMAIL_AND_PASSWORD;
-      }
-
-      const { accessToken } = await response.json();
-      jwtToken.setToken(COOKIE_KEY.JWT_TOKEN, accessToken);
-      user.setAuthorization();
-
-      showSnackBar(SNACKBAR_MESSAGE.SUCCESS.LOGIN);
-    } catch (error) {
-      console.error(error);
-      alert(error);
-    } finally {
-      router.navigate(PATH.ROOT);
-    }
+      router.navigate(PATH.SIGNUP);
+    });
   }
 
   async loginHandler(e) {
@@ -57,23 +46,26 @@ class LoginPage {
     await this.requestLogin(loginData);
   }
 
-  bindCheckInputEvents() {
-    $('#email').addEventListener('keyup', checkEmailInputHandler.bind(this));
-    $('#password').addEventListener(
-      'keyup',
-      checkPasswordInputHandler.bind(this)
-    );
-  }
+  async requestLogin(request) {
+    try {
+      const response = await fetchLogin(request);
 
-  bindEvents() {
-    this.bindCheckInputEvents();
+      if (!response.ok) {
+        throw MESSAGE.ERROR.CHECK_EMAIL_AND_PASSWORD;
+      }
 
-    $('#login-form').addEventListener('submit', this.loginHandler.bind(this));
-    $('#signup').addEventListener('click', e => {
-      e.preventDefault();
+      const { accessToken } = await response.json();
 
-      router.navigate(PATH.SIGNUP);
-    });
+      Cookies.set(COOKIE_KEY.JWT_TOKEN, accessToken);
+      user.setAuthorization();
+
+      showSnackBar(SNACKBAR_MESSAGE.SUCCESS.LOGIN);
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    } finally {
+      router.navigate(PATH.ROOT);
+    }
   }
 }
 
