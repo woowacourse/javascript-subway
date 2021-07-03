ba92f3287c0de1604de288be3fa027ca5fe98fd0
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-07-03 14:06:22 +0900

    refactor: js-cookies 라이브러리통해서 jwtToken 바로 가져오도록 로직 수정

--- src/js/pages/main/MainPage.js
+++ src/js/pages/main/MainPage.js
@@ -1,7 +1,7 @@
+import Cookies from 'js-cookie';
 import { SNACKBAR_MESSAGE } from '../../constants/messages.js';
 import { COOKIE_KEY } from '../../constants/constants.js';
 import { PATH } from '../../constants/path.js';
-import jwtToken from '../../jwtToken.js';
 import { logoutButtonTemplate } from './templates/appNavbar.js';
 import headerTemplate from './templates/header.js';
 import { $ } from '../../utils/DOM.js';
@@ -19,34 +19,15 @@ class MainPage {
     this.bindEvents();
   }
 
-  renderView() {
-    this.$appNavbar.innerHTML = logoutButtonTemplate;
-    this.$navigation.innerHTML = headerTemplate;
-    $('#main').innerHTML = '';
-  }
-
   resetView() {
     this.$appNavbar.innerHTML = '';
     this.$navigation.innerHTML = '';
   }
 
-  logoutHandler(e) {
-    e.preventDefault();
-
-    this.resetView();
-
-    jwtToken.deleteToken(COOKIE_KEY.JWT_TOKEN);
-    // user.authorization = '';
-    showSnackBar(SNACKBAR_MESSAGE.SUCCESS.LOGOUT);
-    router.navigate(PATH.ROOT);
-  }
-
-  navigateHandler(e) {
-    e.preventDefault();
-    if (!e.target.dataset.navPath) return;
-
-    const targetPath = e.target.dataset.navPath;
-    router.navigate(targetPath);
+  renderView() {
+    this.$appNavbar.innerHTML = logoutButtonTemplate;
+    this.$navigation.innerHTML = headerTemplate;
+    $('#main').innerHTML = '';
   }
 
   bindEvents() {
@@ -56,6 +37,26 @@ class MainPage {
     );
 
     this.$navigation.addEventListener('click', this.navigateHandler.bind(this));
+  }
+
+  navigateHandler(e) {
+    e.preventDefault();
+
+    const targetPath = e.target.dataset.navPath;
+
+    if (!targetPath) return;
+    router.navigate(targetPath);
+  }
+
+  logoutHandler(e) {
+    e.preventDefault();
+
+    this.resetView();
+
+    Cookies.remove(COOKIE_KEY.JWT_TOKEN);
+    showSnackBar(SNACKBAR_MESSAGE.SUCCESS.LOGOUT);
+
+    router.navigate(PATH.ROOT);
   }
 }
 
